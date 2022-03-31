10ee446b65d68abf73bc8933f152c527e6b21fda
Tom van den Berg <tomvandenberg11@hotmail.com>
2022-03-31 11:56:07 +0200

    changing port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const store = require('store2')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // Link the templating engine to the express app
 app.set('view engine', 'ejs');
